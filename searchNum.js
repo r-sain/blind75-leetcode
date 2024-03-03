3783ddc4610b4df79ed0181b9b14b0a8c7d2e886
Rohit Sain <102957043+r-sain@users.noreply.github.com>
2024-03-03 15:16:01 +0530

    33. Search in Rotated Sorted Array

---
+++ searchNum.js
@@ -0,0 +1,20 @@
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) return mid;
+
+    //right segment is sorted?
+    if (nums[right] > nums[mid]) {
+      if (target > nums[mid] && target <= nums[right]) left = mid + 1;
+      else right = mid - 1;
+    } else {
+      if (target < nums[mid] && target >= nums[left]) right = mid - 1;
+      else left = mid + 1;
+    }
+  }
+  return -1;
+};
