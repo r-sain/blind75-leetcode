3bc308319d5b4d061abc898e03c3c29e07bec73e
Rohit Sain <102957043+r-sain@users.noreply.github.com>
2024-03-17 16:18:22 +0530

    20. Valid Parentheses

---
+++ validParanthesis.js
@@ -0,0 +1,13 @@
+var isValid = function (s) {
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') stack.push(')');
+    else if (s[i] === '{') stack.push('}');
+    else if (s[i] === '[') stack.push(']');
+    else if (stack.pop() !== s[i]) return false;
+  }
+
+  if (stack.length === 0) return true;
+  else return false;
+};
